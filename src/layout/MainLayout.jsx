0f96eef00ca3bec636f9504c07943f7fae216cf9
YogendraMLIM <113332279+YogendraMLIM@users.noreply.github.com>
2024-07-29 17:22:11 +0530

    initial commit

--- src/layout/MainLayout.jsx
+++ src/layout/MainLayout.jsx
@@ -1,37 +1,28 @@
 // src/layouts/MainLayout.js
 import React from 'react';
-import { useSelector, useDispatch } from 'react-redux';
 import { Box, Toolbar } from '@mui/material';
 import AppBar from '../basic/AppBar';
 import AppDrawer from '../basic/AppDrawer'
 import AppFooter from '../basic/AppFooter'
 import { Outlet } from 'react-router-dom';
-const drawerWidth = 240;
 
 const MainLayout = () => {
-    const drawerShow = useSelector((state) => state.drawerShow)
-    const dispatch = useDispatch();
+  const [drawerOpen, setDrawerOpen] = React.useState(false);
 
   const handleDrawerToggle = () => {
-    dispatch({ type: 'set', drawerShow: !drawerShow })
+    setDrawerOpen(!drawerOpen);
   };
 
   return (
-    <Box sx={{ 
-        display: 'flex', 
-        flexDirection: 'column', 
-        minHeight: '100vh',
-        ml: drawerShow ? 0 : '-240px'
-        }}
-        >
-      <AppBar open={drawerShow} onMenuClick={handleDrawerToggle} />
-      <AppDrawer open={drawerShow} onClose={handleDrawerToggle} />
+    <Box sx={{ display: 'flex', flexDirection: 'column', minHeight: '100vh' }}>
+      <AppBar onMenuClick={handleDrawerToggle} />
+      <AppDrawer open={drawerOpen} onClose={handleDrawerToggle} />
       <Box
         component="main"
         sx={{
           flexGrow: 1,
           p: 3,
-          ml: drawerShow ? 0 : `-${drawerWidth}px`,
+          ml: { sm: `${240}px` }, // Adjust margin based on drawer width
         }}
       >
         <Toolbar />
